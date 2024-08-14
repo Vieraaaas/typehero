e9c3c87c80407c96a15ad904d8a5d11f4e43c78a
Claas Paaschburg <paaschburgc@gmail.com>
2024-08-14 11:19:25 +0200

    chore: set types via typeof operator

--- typeof-operator/script.ts
+++ typeof-operator/script.ts
@@ -1,6 +1,8 @@
 type Width = typeof width;
-type Margin = unknown;
-type Data = unknown;
-type YScale = unknown;
+type Margin = typeof margin;
+type Data = typeof d3ChartConfig.data;
+type YScale = typeof d3ChartConfig.yScale;
 
-type D3ChartConfig = unknown;
+type D3ChartConfig = typeof d3ChartConfig;
+
+// varaiables are included in the tests at https://typehero.dev/challenge/typeof
