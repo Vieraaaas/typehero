1783b29306686d39f4cd25dd350ab2ce4e1310a5
Claas Paaschburg <paaschburgc@gmail.com>
2024-08-15 17:07:11 +0200

    chore: add new types via indexed types

--- indexed-types/script.ts
+++ indexed-types/script.ts
@@ -1,2 +1,11 @@
-type TheCoolestCarEverMade = unknown;
-type TruckDriverBonusGiver = unknown;
+type Cars = ["Bugatti", "Ferarri", "Lambo", "Porsche", "Toyota Corolla"];
+type TheCoolestCarEverMade = Cars[4];
+
+type Donations = {
+  Bono: 15_000_000;
+  "J.K. Rowling": 160_000_000;
+  "Taylor Swift": 45_000_000;
+  "Elton John": 600_000_000;
+  "Angelina Jolie and Brad Pitt": 100_000_000;
+};
+type TruckDriverBonusGiver = Donations["Taylor Swift"];
